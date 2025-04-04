59cccb863b0c0172ff51376fba7f30ca5207cb67
Jacob Root <me@rooty.dev>
2025-04-03 23:58:11 -0700

    change

--- src/routes/marvin_webhooks.rs
+++ src/routes/marvin_webhooks.rs
@@ -1,17 +1,31 @@
 use axum::{
+    Json, Router,
     body::Body,
     http::{Request, StatusCode},
     middleware::{self, Next},
     response::Response,
-    Json, Router,
     routing::post,
 };
 use serde::Deserialize;
 use serde_json::Value;
-use tokio::time::{sleep, Sleep};
 use std::{env, sync::Arc, time::Duration};
+use tokio::time::{Sleep, sleep};
 
-use crate::{api::{client::MarvinClient, requests::{CreateProjectRequest, CreateTaskRequest}}, cache::cache::{self, cache_get, cache_put, TOGGL_CLIENT_CACHE, TOGGL_PROJECT_CACHE, TOGGL_TASK_CACHE}, models::tasks::{ProjectOrCategory, Task}, toggl_api::{client::TogglClient, requests::{CreateClientRequest, CreateTagRequest}}, WORKSPACE_ID};
+use crate::{
+    WORKSPACE_ID,
+    api::{
+        client::MarvinClient,
+        requests::{CreateProjectRequest, CreateTaskRequest},
+    },
+    cache::cache::{
+        self, TOGGL_CLIENT_CACHE, TOGGL_PROJECT_CACHE, TOGGL_TASK_CACHE, cache_get, cache_put,
+    },
+    models::tasks::{ProjectOrCategory, Task},
+    toggl_api::{
+        client::TogglClient,
+        requests::{CreateClientRequest, CreateTagRequest},
+    },
+};
 
 /// Main router for Marvin webhooks.
 pub fn router() -> Router {
@@ -27,7 +41,7 @@ pub fn router() -> Router {
 /// Check if the request has a valid "Authorization" header that matches
 /// the `MARVIN_WEBHOOK_TOKEN` environment variable.
 async fn require_auth(req: Request<Body>, next: Next) -> Result<Response, StatusCode> {
-    // Load the secret token from env (once per request). If it's missing from env, 
+    // Load the secret token from env (once per request). If it's missing from env,
     // return 500 — or handle this differently, e.g. panic at startup so you know immediately.
     let token = match env::var("MARVIN_WEBHOOK_TOKEN") {
         Ok(val) => val,
@@ -61,8 +75,8 @@ async fn start_tracking(Json(payload): Json<Task>) -> Result<String, StatusCode>
 
     let marvin_api_token = match env::var("MARVIN_API_TOKEN") {
         Ok(val) => val,
-        
-Err(_) => {
+
+        Err(_) => {
             eprintln!("MARVIN_API_TOKEN is not set!");
             return Err(StatusCode::INTERNAL_SERVER_ERROR);
         }
@@ -89,37 +103,34 @@ Err(_) => {
     // TODO: remove localhost override
     let marvin_client = MarvinClient::new(Some(marvin_api_token), Some(marvin_full_access_token));
 
-
     while parent_id != "root" && parent_id != "unassigned" {
         // Get project from cache
         let parent = cache::cache_get(Arc::clone(&*cache::MARVIN_PROJECT_CACHE), &parent_id);
         let parent = match parent {
             Some(parent) => parent,
-            None => {
-                match marvin_client.read_doc(&parent_id).await {
-                    Ok(doc) => {
-                        let title = match doc.extra.get("title") {
-                            Some(Value::String(title)) => title.clone(),
-                            _ => {
-                                return Err(StatusCode::SERVICE_UNAVAILABLE)
-                            }
-                        };
-                        let parent = match doc.extra.get("parentId") {
-                            Some(Value::String(parent)) => parent.clone(),
-                            _ => {
-                                return Err(StatusCode::SERVICE_UNAVAILABLE)
-                            }
-                        };
-                        (title, parent)
-                    },
-                    Err(err) => {
-                        println!("{}", err);
-                        return Err(StatusCode::SERVICE_UNAVAILABLE)
-                    },
+            None => match marvin_client.read_doc(&parent_id).await {
+                Ok(doc) => {
+                    let title = match doc.extra.get("title") {
+                        Some(Value::String(title)) => title.clone(),
+                        _ => return Err(StatusCode::SERVICE_UNAVAILABLE),
+                    };
+                    let parent = match doc.extra.get("parentId") {
+                        Some(Value::String(parent)) => parent.clone(),
+                        _ => return Err(StatusCode::SERVICE_UNAVAILABLE),
+                    };
+                    (title, parent)
                 }
-            }
+                Err(err) => {
+                    println!("{}", err);
+                    return Err(StatusCode::SERVICE_UNAVAILABLE);
+                }
+            },
         };
-        cache::cache_put(Arc::clone(&*cache::MARVIN_PROJECT_CACHE), parent_id, parent.clone());
+        cache::cache_put(
+            Arc::clone(&*cache::MARVIN_PROJECT_CACHE),
+            parent_id,
+            parent.clone(),
+        );
         parents.push(parent.0);
         parent_id = parent.1;
         // Make the marvin rate limiter happy by waiting 1 second
@@ -149,9 +160,13 @@ Err(_) => {
                             if id == l.id {
                                 result = l.title.to_string();
                             }
-                            cache::cache_put(Arc::clone(&*cache::MARVIN_LABEL_CACHE), l.id, l.title);
+                            cache::cache_put(
+                                Arc::clone(&*cache::MARVIN_LABEL_CACHE),
+                                l.id,
+                                l.title,
+                            );
                         }
-                    },
+                    }
                     Err(err) => {
                         println!("Error collecting labels: {}", err);
                         return Err(StatusCode::INTERNAL_SERVER_ERROR);
@@ -179,7 +194,7 @@ Err(_) => {
                             }
                             cache::cache_put(Arc::clone(&*cache::TOGGL_TAG_CACHE), l.name, l.id);
                         }
-                    },
+                    }
                     Err(err) => {
                         println!("Error collecting tags: {}", err);
                         return Err(StatusCode::INTERNAL_SERVER_ERROR);
@@ -187,12 +202,18 @@ Err(_) => {
                 }
 
                 if result == -1 {
-                    let tag_request = CreateTagRequest {name: label.clone()};
+                    let tag_request = CreateTagRequest {
+                        name: label.clone(),
+                    };
                     match toggl_client.create_tag(*workspace_id, &tag_request).await {
                         Ok(tag) => {
                             result = tag.id;
-                            cache::cache_put(Arc::clone(&*cache::TOGGL_TAG_CACHE), tag.name, tag.id);
-                        },
+                            cache::cache_put(
+                                Arc::clone(&*cache::TOGGL_TAG_CACHE),
+                                tag.name,
+                                tag.id,
+                            );
+                        }
                         Err(err) => {
                             println!("Error adding tag: {}", err);
                             return Err(StatusCode::INTERNAL_SERVER_ERROR);
@@ -208,8 +229,6 @@ Err(_) => {
         }
     }
 
-
-
     println!("Tags: {:#?}", tags);
 
     // Length 0: description is what is being done
@@ -222,7 +241,7 @@ Err(_) => {
         let mut client = &parents[0];
         let mut project = &parents[0];
 
-        let mut task = &payload.title;
+        let mut task = None;
         let description = &payload.title;
         if parents.len() == 1 {
             client = &parents[0];
@@ -233,19 +252,19 @@ Err(_) => {
             project = &parents[0];
         }
         if parents.len() == 3 {
-            task = &parents[0];
+            task = Some(&parents[0]);
             project = &parents[1];
             client = &parents[2];
         }
         if parents.len() > 3 {
-            task = &parents[0];
-            project = &parents[1];
+            task = Some(&parents[0]);
+            project = &parents[parents.len() - 2];
             client = &parents[parents.len() - 1];
         }
 
         let client = &str::trim(client).to_string();
         let project = &str::trim(project).to_string();
-        let task = &str::trim(task).to_string();
+        let task = task.map(|x| str::trim(x).to_string());
         let description = &str::trim(description).to_string();
 
         let client = match cache_get(Arc::clone(&*TOGGL_CLIENT_CACHE), &client) {
@@ -257,14 +276,18 @@ Err(_) => {
                     Err(error) => {
                         println!("Error fetching clients {}", error);
                         return Err(StatusCode::INTERNAL_SERVER_ERROR);
-                    },
+                    }
                 };
                 let mut our_client: Option<i64> = None;
                 for returned_client in clients {
                     if *client == returned_client.name {
                         our_client = Some(returned_client.id);
                     }
-                    cache_put(Arc::clone(&*TOGGL_CLIENT_CACHE), returned_client.name, returned_client.id);
+                    cache_put(
+                        Arc::clone(&*TOGGL_CLIENT_CACHE),
+                        returned_client.name,
+                        returned_client.id,
+                    );
                 }
                 match our_client {
                     Some(client) => client,
@@ -272,7 +295,7 @@ Err(_) => {
                         // Create our own
                         let request = &CreateClientRequest {
                             name: client.to_string(),
-                            notes: None
+                            notes: None,
                         };
                         let result = toggl_client.create_client(*workspace_id, request).await;
                         match result {
@@ -287,7 +310,10 @@ Err(_) => {
             }
         };
 
-        let project = match cache_get(Arc::clone(&*TOGGL_PROJECT_CACHE), &(client, (*project).clone())) {
+        let project = match cache_get(
+            Arc::clone(&*TOGGL_PROJECT_CACHE),
+            &(client, (*project).clone()),
+        ) {
             Some(project) => project,
             None => {
                 // List of clients and use that; if not, create our own
@@ -296,23 +322,30 @@ Err(_) => {
                     Err(error) => {
                         println!("Error fetching projects {}", error);
                         return Err(StatusCode::INTERNAL_SERVER_ERROR);
-                    },
+                    }
                 };
                 let mut our_project: Option<i64> = None;
                 for returned_project in projects {
-                    if *project  == returned_project.name && returned_project.client_id == Some(client) {
+                    if *project == returned_project.name
+                        && returned_project.client_id == Some(client)
+                    {
                         our_project = Some(returned_project.id);
                     }
                     match returned_project.client_id {
-                        Some(client) => cache_put(Arc::clone(&*TOGGL_PROJECT_CACHE), (client, returned_project.name), returned_project.id),
-                        None => ()
+                        Some(client) => cache_put(
+                            Arc::clone(&*TOGGL_PROJECT_CACHE),
+                            (client, returned_project.name),
+                            returned_project.id,
+                        ),
+                        None => (),
                     };
                 }
                 match our_project {
                     Some(project) => project,
                     None => {
                         // Create our own
-                        let mut request: crate::toggl_api::requests::CreateProjectRequest = Default::default();
+                        let mut request: crate::toggl_api::requests::CreateProjectRequest =
+                            Default::default();
                         request.active = Some(true);
                         request.auto_estimates = Some(false);
                         request.billable = Some(false);
@@ -333,45 +366,57 @@ Err(_) => {
             }
         };
 
-        let task = match cache_get(Arc::clone(&*TOGGL_TASK_CACHE), &(project, (*task).clone())) {
-            Some(task) => task,
-            None => {
-                // List of clients and use that; if not, create our own
-                let tasks = match toggl_client.get_project_tasks(*workspace_id, project).await {
-                    Ok(clients) => clients,
-                    Err(error) => {
-                        println!("Error fetching tasks {}", error);
-                        return Err(StatusCode::INTERNAL_SERVER_ERROR);
-                    },
-                };
-                let mut our_task: Option<i64> = None;
-                for returned_task in tasks {
-                    if *task  == returned_task.name {
-                        our_task = Some(returned_task.id);
-                    }
-                    cache_put(Arc::clone(&*TOGGL_TASK_CACHE), (project, returned_task.name), returned_task.id);
-                }
-                match our_task {
-                    Some(task) => task,
+        let task = match task {
+            Some(task) => {
+                match cache_get(Arc::clone(&*TOGGL_TASK_CACHE), &(project, (task).clone())) {
+                    Some(task) => Some(task),
                     None => {
-                        // Create our own
-                        let request = &crate::toggl_api::requests::CreateTaskRequest {
-                            active: Some(true),
-                            estimated_seconds: Some(0),
-                            name: (*task).clone(),
-                            user_id: None,
-                        };
-                        let result = toggl_client.create_task(*workspace_id, project, request).await;
-                        match result {
-                            Ok(task) => task.id,
-                            Err(error) => {
-                                println!("Error creating tasks {}", error);
-                                return Err(StatusCode::INTERNAL_SERVER_ERROR);
+                        // List of clients and use that; if not, create our own
+                        let tasks =
+                            match toggl_client.get_project_tasks(*workspace_id, project).await {
+                                Ok(clients) => clients,
+                                Err(error) => {
+                                    println!("Error fetching tasks {}", error);
+                                    return Err(StatusCode::INTERNAL_SERVER_ERROR);
+                                }
+                            };
+                        let mut our_task: Option<i64> = None;
+                        for returned_task in tasks {
+                            if task == returned_task.name {
+                                our_task = Some(returned_task.id);
+                            }
+                            cache_put(
+                                Arc::clone(&*TOGGL_TASK_CACHE),
+                                (project, returned_task.name),
+                                returned_task.id,
+                            );
+                        }
+                        match our_task {
+                            Some(task) => Some(task),
+                            None => {
+                                // Create our own
+                                let request = &crate::toggl_api::requests::CreateTaskRequest {
+                                    active: Some(true),
+                                    estimated_seconds: Some(0),
+                                    name: (task).clone(),
+                                    user_id: None,
+                                };
+                                let result = toggl_client
+                                    .create_task(*workspace_id, project, request)
+                                    .await;
+                                match result {
+                                    Ok(task) => Some(task.id),
+                                    Err(error) => {
+                                        println!("Error creating tasks {}", error);
+                                        return Err(StatusCode::INTERNAL_SERVER_ERROR);
+                                    }
+                                }
                             }
                         }
                     }
                 }
             }
+            None => None,
         };
 
         let result = toggl_client.stop_current_time_entry(None).await;
@@ -381,13 +426,15 @@ Err(_) => {
             Ok(_) => (),
         }
 
-
-        match toggl_client.start_time_entry(*workspace_id, Some(project), Some(task), description, tags).await {
+        match toggl_client
+            .start_time_entry(*workspace_id, Some(project), task, description, tags)
+            .await
+        {
             Err(error) => {
                 println!("Start time entry error: {}", error);
                 return Err(StatusCode::INTERNAL_SERVER_ERROR);
             }
-            Ok(_) => ()
+            Ok(_) => (),
         }
     } else {
         let result = toggl_client.stop_current_time_entry(None).await;
@@ -397,13 +444,15 @@ Err(_) => {
             Ok(_) => (),
         }
 
-
-        match toggl_client.start_time_entry(*workspace_id, None, None, payload.title.as_str(), tags).await {
+        match toggl_client
+            .start_time_entry(*workspace_id, None, None, payload.title.as_str(), tags)
+            .await
+        {
             Err(error) => {
                 println!("start time entry error: {}", error);
                 return Err(StatusCode::INTERNAL_SERVER_ERROR);
             }
-            Ok(_) => ()
+            Ok(_) => (),
         }
     }
 
@@ -458,9 +507,13 @@ async fn stop_tracking(Json(payload): Json<Task>) -> Result<String, StatusCode> 
                             if id == l.id {
                                 result = l.title.to_string();
                             }
-                            cache::cache_put(Arc::clone(&*cache::MARVIN_LABEL_CACHE), l.id, l.title);
+                            cache::cache_put(
+                                Arc::clone(&*cache::MARVIN_LABEL_CACHE),
+                                l.id,
+                                l.title,
+                            );
                         }
-                    },
+                    }
                     Err(err) => {
                         println!("Error collecting labels: {}", err);
                         return Err(StatusCode::INTERNAL_SERVER_ERROR);
@@ -487,7 +540,9 @@ async fn stop_tracking(Json(payload): Json<Task>) -> Result<String, StatusCode> 
         }
     }
 
-    let result = toggl_client.stop_current_time_entry(productivity_override).await;
+    let result = toggl_client
+        .stop_current_time_entry(productivity_override)
+        .await;
 
     match result {
         Err(error) => println!("Stop current time entry error: {}", error),
